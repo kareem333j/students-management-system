0d78b70979b6c2e77281f6751e0a5c69d4df105c
karim magdy <kareem147j@gmail.com>
2025-09-15 23:09:17 +0300

    allow vercel host

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG") == "False"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
 
 
