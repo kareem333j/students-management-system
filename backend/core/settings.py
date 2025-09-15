8ed24e33664fad001a2872ac725003b7532a9ced
karim magdy <kareem147j@gmail.com>
2025-09-15 23:38:59 +0300

    update vercel file bug2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -71,9 +71,19 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv("POSTGRES_URL"),
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
